2b32f3caffe013544db56d699217b27f3eee6c5c
Matt <mfxod828@gmail.com>
2019-08-27 20:29:42 -0400

    added GET routes

--- routes/html.js
+++ routes/html.js
@@ -3,16 +3,44 @@ const express = require('express')
 const router = express.Router()
 
 router
+// DUSTIN'S TEST
+// .get('/', (_, res) => {
+//   res.render('index', {
+//     title: 'My Cool App',
+//     user: 'Nerd'
+//   })
+// })
+
+// LOG IN
 .get('/', (_, res) => {
   res.render('index', {
-    title: 'My Cool App',
-    user: 'Nerd'
+    title: 'Log In',
+    body: 'components/login'
   })
 })
 
-// TESTING GET
-.get('/login', (_, res) => {
-  res.render('login')
+// HOME
+.get('/home', (_, res) => {
+  res.render('index', {
+    title: 'Home',
+    body: 'components/home'
+  })
+})
+
+// VIEW JOB
+.get('/view-job', (_, res) => {
+  res.render('index', {
+    title: 'View Job',
+    body: 'components/view-job'
+  })
+})
+
+// ADD JOB
+.get('/add-job', (_, res) => {
+  res.render('index', {
+    title: 'Add a New Job',
+    body: 'components/add-job'
+  })
 })
 
 module.exports = router
