af97157526e1077c8847ebfe43a16635200a0ff4
sinid <49916315+oinksu@users.noreply.github.com>
2019-09-04 17:07:14 -0400

    added models folder with JS files and placeholder data  corresponding models of tables in the DB

---
+++ models/Recipient.js
@@ -0,0 +1,34 @@
+// const Sequelize = require('sequelize')
+// const db = require('../config/database')
+
+// const Gig = db.define('gig', {
+//   customer_name: {
+//     type: Sequelize.STRING
+//   },
+//   contact_first_name: {
+//     type: Sequelize.STRING
+//   },
+//   contact_last_name: {
+//     type: Sequelize.STRING
+//   },
+//   contact_email: {
+//     type: Sequelize.STRING
+//   },
+//   contact_phone: {
+//     type: Sequelize.STRING
+//   },
+//   street_address: {
+//     type: Sequelize.STRING
+//   },
+//   city: {
+//     type: Sequelize.STRING
+//   },
+//   state: {
+//     type: Sequelize.STRING
+//   },
+//   zip: {
+//     type: Sequelize.STRING
+//   }
+// })
+
+// module.exports = Gig
