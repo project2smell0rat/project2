757eed2a0c08f2ab004be2fac519d48ba8d08d22
sinid <49916315+oinksu@users.noreply.github.com>
2019-09-04 22:44:20 -0400

    deleted database folder and index file within it. Added sequilize configuration in config/database.js  and added database connection validation in the app.use file  app.usedb commented out until middleware is established.  app.js file was formaly known as index.js. Configured packge file to run

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 require('dotenv').config()
-const path = require('path')
 const express = require('express')
-const db = require('./db')
+const bodyParser = require('body-parser')
+
+const path = require('path')
+
+const db = require('./config/database')
+
 const htmlRoutes = require('./routes/html')
 const apiRoutes = require('./routes/api')
+
+// added by Felipe for connection
+// test db connection: returns a pormise tests connection with verification and error (the database configuration lives in the config folder->database.js)
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err))
 
 const app = express()
 const PORT = process.env.PORT || 3030
@@ -14,7 +24,7 @@ app
   .use(express.urlencoded({ extended: false }))
   .use(express.json())
   .use(express.static(path.join(__dirname, 'public')))
-  .use(db)
+  // .use(db)
   .use(htmlRoutes)
   .use(apiRoutes)
   .listen(PORT, () => {
--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
-// // declaring sequelize, code pasted form package documentation page makes conection between sequelize models and mysqldb
-// const Sequelize = require('sequelize')
+// DATABASE CONNECTED!! SHOULD NOT NEED TO CHANGE THIS FILE
 
-// // Option 1: Passing parameters separately. codegi refers to the database I'm using root it is db username password its password copied code declares constant as sequelize I changed it to db to follow example the constant from the original code was change to module.export to export it to other files
-// module.exports = new Sequelize('ratsmeller', 'root', 'password', {
-//   host: 'localhost',
-//   dialect: 'mysql',
+// declaring sequelize, code pasted form package documentation page
+const Sequelize = require('sequelize');
 
-//   // pool section was not altered in example don't know yet what this would be used for
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// })
+// Option 1: Passing parameters separately. codegi refers to the database I'm using root it is db username password its password copied code declares constant as sequelize I changed it to db to follow example the constant from the original code was change to module.export to export it to other files
+module.exports = new Sequelize('codegig', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  // pool section was not altered in example don't know yet what this would be used for
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
